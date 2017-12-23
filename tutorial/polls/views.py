2ad0ac6da61d50b12c0408d10a0bc480253530e8
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2017-12-23 10:28:49 -0500

    Update views.py

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -21,4 +21,15 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HHttpResponseRedirect(reverse('polls:results', argas=(question.id,)))
