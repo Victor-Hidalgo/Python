79e4cd5c45ffbb512415fc0d4c843001ec32e037
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2017-12-23 11:03:05 -0500

    Update views.py

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HHttpResponseRedirect
+from django.views import generic
 from django.template import loader
 from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question
 # Create your views here.
