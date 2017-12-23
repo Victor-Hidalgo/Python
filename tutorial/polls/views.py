c48f7e4f5af493d71c010048c7537d586a8ffe1f
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2017-12-23 09:29:28 -0500

    Update views.py

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -13,10 +13,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
