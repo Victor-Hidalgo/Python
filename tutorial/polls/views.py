997cd4238923fca5e05a664ec650c8ad6a211a23
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2017-12-24 14:11:42 -0500

    Update views.py

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,32 +1,47 @@
 from django.shortcuts import render
-from django.http import HHttpResponseRedirect
-from django.views import generic
-from django.template import loader
-from django.http import Http404
+from django.http      import HttpResponseRedirect
+from django.views     import generic
+from django.template  import loader
+from django.http      import Http404
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-
-from .models import Question
+from django.urls      import reverse
+from .models          import Choice, Question
+from django.utils     import timezone
 # Create your views here.
 
+class IndexView(generic.ListView):
+    template_name       = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        """Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list,}
-    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    response = "You're looking at the results of question /s."
-    return HttpResponse(response % question_id)
-
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+    """
+    
+    Excludes any questions that aren't published yet.
+    """
+    
+    return Question.objects.filter(pub_date__lte=timezone.now())
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
@@ -35,4 +50,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HHttpResponseRedirect(reverse('polls:results', argas=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id,)))
