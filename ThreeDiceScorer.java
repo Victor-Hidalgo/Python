426c9b78aa10313b3a196e3d5352f2891fee677f
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-03-04 12:57:22 +0000

    Update ThreeDiceScorer.java

--- ThreeDiceScorer.java
+++ ThreeDiceScorer.java
@@ -2,31 +2,24 @@ import java.util.*;
 
 public class ThreeDiceScorer extends ThreeDice {
     
-    private int die1;
-    private int die2;
-    private int die3;
     
     public ThreeDiceScorer (int s1, int s2, int s3) {
         super (s1, s2, s3);
     }
     
  
-    public void Calculus () {
-        System.out.print(getDie1() + " " + getDie2() + " " + getDie3() + "   ");
+    public int Calculus () {
         
-        int sumOfDice;
+        int sumOfDice = 0;
         
-        if (threeSame()) {sumOfDice = getDie1() + getDie2() + getDie3() + 60;
-        System.out.println ("Points: " + sumOfDice);}
+        if (threeSame()) {sumOfDice = getDie1() + getDie2() + getDie3() + 60;}
         
-        else if (runOfThree()) {sumOfDice = getDie1() + getDie2() + getDie3() + 40;
-        System.out.println ("Points: " + sumOfDice);}
+        else if (runOfThree()) {sumOfDice = getDie1() + getDie2() + getDie3() + 40;}
         
-        else if (pair()) {sumOfDice = getDie1() + getDie2() + getDie3() + 20;
-        System.out.println ("Points: " + sumOfDice);}
+        else if (pair()) {sumOfDice = getDie1() + getDie2() + getDie3() + 20;}
         
         else if (allDifferent()) {sumOfDice = getDie1() + getDie2() + getDie3();
-        System.out.println ("Points: " + sumOfDice);}
+        }
         
+        return sumOfDice;}    
     }
-}
