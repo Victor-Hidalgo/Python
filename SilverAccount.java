f199cc4c6df1408fd1add66ec6d59351009c9448
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-03-06 15:42:41 +0000

    Update SilverAccount.java

--- SilverAccount.java
+++ SilverAccount.java
@@ -1,18 +1,43 @@
 import java.util.*;
 
-public class SilverAccount extends StandardAccount { 
-	
-    public SilverAccount(int DaytimeMins, int WeekendMins, int MegabytesUsed, double DaytimeCost, double WeekendCost, double PackageCost, int Channels, int Broadband, double BroadbandCost){
-        super(DaytimeMins, WeekendMins, MegabytesUsed, DaytimeCost, WeekendCost, PackageCost, Channels,Broadband,BroadbandCost);
+public class SilverAccount extends StandardAccount {
+    
+    public SilverAccount(int DaytimeMins, int WeekendMins, int MegabytesUsed){
+        super(DaytimeMins, WeekendMins, MegabytesUsed);
     }
     
-    public void getAccount() {
-    
-	 System.out.println("Account Summary for Silver Account \n Package Cost: " + getPackageCost() + "\n Cost of daytime calls: " + getDaytimeCost()
-        + "/min \n Cost of evening and weekend calls: " + getWeekendCost() + "/min \n Number of Channels: " + getChannels()
-        + "\n Broadband Included: " + getBroadband() + "Mb \n Broadband Cost (above included limit): " + getBroadbandCost()
-        + "/Mb \n Total daytime calls cost: " + TotalDaytime() + "\n Total evening calls: " + TotalWeekend() + "\n Total (extra) broadband cost: "
-        + TotalBroadband() + "\n Total cost: " + TotalCost() + "\n Spotify Account provided");
+    public String getName(){
         
+        return "Account Summary for Silver Account";
+    }
+    
+    public double DaytimeCost(){
+        
+        return 0.12;
+    }
+    
+    public double WeekendCost(){
+        
+        return 0.00;
+    }
+    
+    public double PackageCost(){
+        
+        return 46.00;
+    }
+    
+    public int Channels(){
+        
+        return 130;
+    }
+    
+    public int Broadband(){
+        
+        return 1000;
+    }
+    
+    public double BroadbandCost(){
+        
+        return 0.01;
     }
 }
