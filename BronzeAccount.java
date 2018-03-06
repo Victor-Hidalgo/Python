d36b8ac8fc9680a5e8a7ee1a7c13c117347c99d2
Victorahr99 <33233227+Victorahr99@users.noreply.github.com>
2018-03-06 14:44:49 +0000

    Update BronzeAccount.java

--- BronzeAccount.java
+++ BronzeAccount.java
@@ -1,25 +1,19 @@
 import java.util.*;
 
-public class BronzeAccount extends StandardAccount {
-    
-    protected int DaytimeMins;
-    protected int WeekendMins;
-    protected int MegabytesUsed;
-    protected double DaytimeCost = 0.12;
-    protected double WeekendCost = 0.05;
-    protected double PackageCost = 36.00;
-    protected int Channels = 60;
-    protected int Broadband = 500;
-    protected double BroadbandCost = 0.02;
-    protected String Name = "Account Summary for Broze Account";
-    
-    public BronzeAccount(DaytimeMins, WeekendMins, MegabytesUsed){
-        super(DaytimeMins, WeekendMins, MegabytesUsed);
+public class BronzeAccount extends StandardAccount { 
+	
+    public BronzeAccount(int DaytimeMins, int WeekendMins, int MegabytesUsed, double DaytimeCost, double WeekendCost, double PackageCost, int Channels, int Broadband, double BroadbandCost){
+        super(DaytimeMins, WeekendMins, MegabytesUsed, DaytimeCost, WeekendCost, PackageCost, Channels,Broadband,BroadbandCost);
     }
     
-    public String getName() {
-        
-        return Name;
+    public void getAccount() {
+    
+	 System.out.println("Account Summary for Bronze Account \n Package Cost: " + getPackageCost() + "\n Cost of daytime calls: " + getDaytimeCost()
+        + "/min \n Cost of evening and weekend calls: " + getWeekendCost() + "/min \n Number of Channels: " + getChannels()
+        + "\n Broadband Included: " + getBroadband() + "Mb \n Broadband Cost (above included limit): " + getBroadbandCost()
+        + "/Mb \n Total daytime calls cost: " + TotalDaytime() + "\n Total evening calls: " + TotalWeekend() + "\n Total (extra) broadband cost: "
+        + TotalBroadband() + "\n Total cost: " + TotalCost());
         
     }
+}
 }
