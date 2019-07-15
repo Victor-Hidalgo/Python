f3e3e59267eab6491032f9510609320a30c43300
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2019-07-15 20:26:49 +0100

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -10,4 +10,9 @@ currentTime = datetime.datetime.now()
 print("\nCurrent date and time:", currentTime.strftime("%Y-%m-%d %H:%M:%S"))
 
 r = float(input ("\nPlease input the radius of the circle: "))
-print("\nThe area of the circle is " + str(pi * r**2))
+print("The area of the circle is " + str(pi * r**2))
+
+#use input() to ask the user for Strings (without specifying the type)
+firstName = input("\nPlease enter your first name: ")
+lastName = input("Please enter your last name: ")
+print("Good evening " + lastName + ", " + firstName)
