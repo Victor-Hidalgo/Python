d2d9032710f3074c7f8ae32bcd1ca22a18bbaa3a
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2019-07-19 12:23:34 +0100

    Update BasicsPartII.py

--- BasicsPartII.py
+++ BasicsPartII.py
@@ -1,2 +1,7 @@
 color_list = ["Red", "Blue", "Yellow", "Black"]
 print(color_list[0], color_list[3])
+
+#Convert a String to an int
+s = int(input("Input an integer: "))
+i = int("%s" % s)
+mult = i * 2
