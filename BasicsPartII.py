59eb109c91cfbf71ed786f6e985d0dbb05275db1
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2019-07-24 21:03:28 +0100

    Update BasicsPartII.py

--- BasicsPartII.py
+++ BasicsPartII.py
@@ -1,9 +1,16 @@
+import calendar
+
 color_list = ["Red", "Blue", "Yellow", "Black"]
 print(color_list[0], color_list[3])
 
 #Convert a String to an int
-s = int(input("Input an integer: "))
-i = int("%s" % s)
+t = int(input("Input an integer: "))
+i = int("%s" % t)
 mult = i * 2
 print(mult)
-#only use the plus sign to combine Strings
+#only use the plus sign to combine strings
+
+yr = int(input("\nPlease input a year: "))
+mth = int(input("Please input a month: "))
+
+print(calendar.month(yr, mth))
