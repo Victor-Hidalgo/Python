3db3fccd2fa3963580f0c206bbf0d487e339379c
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2019-07-24 21:19:55 +0100

    Update BasicsPartII.py

--- BasicsPartII.py
+++ BasicsPartII.py
@@ -1,4 +1,5 @@
 import calendar
+from datetime import date
 
 color_list = ["Red", "Blue", "Yellow", "Black"]
 print(color_list[0], color_list[3])
@@ -14,3 +15,14 @@ yr = int(input("\nPlease input a year: "))
 mth = int(input("Please input a month: "))
 
 print(calendar.month(yr, mth))
+
+f_day = int(input("\nPlease type the start day: "))
+l_day = int(input("Please type the end day: "))
+
+firstDate = date(yr, mth, f_day)
+secondDate = date(yr, mth, l_day)
+
+diff = secondDate - firstDate
+print(diff.days, "days")
+#print(""" Al
+#Contacto""")
