c6ae52f2cd0b840baf21924ad88764e55b995687
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2019-07-26 19:07:01 +0100

    Update Shapes.py

--- Shapes.py
+++ Shapes.py
@@ -2,3 +2,12 @@ pi = 3.1415926536
 r = float(input("Please input the radio: "))
 volume = (4/3) * pi * r**3
 print("The volume of the sphere is:", volume)
+
+def getLocation(n):
+    return ((abs(1000 - n) <= 100) or (abs(2000 - n) <= 100))
+
+#the function returns a boolean
+print(getLocation(1000))
+print(getLocation(900))
+print(getLocation(2200))
+print(getLocation(99))
