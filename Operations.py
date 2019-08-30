e2e406d8aa6c28d29e4ec38e43e9711c96fa8134
Victor Hidalgo <33233227+Victorahr99@users.noreply.github.com>
2019-08-30 14:39:03 +0100

    Update Operations.py

--- Operations.py
+++ Operations.py
@@ -12,3 +12,16 @@ first = int (input("Enter first number: "))
 second = int (input("Enter second number: "))
 
 print(addition(first, second))
+
+def exercise5(x, y):
+
+    if x == y or abs(x - y) == 5 or (x + y) == 5:
+        print("True")
+        
+    else:
+        print("False")
+
+#it is possible to replace the return statements with print statements
+#however, the function must not be called with the print command
+
+exercise5(first, second)
