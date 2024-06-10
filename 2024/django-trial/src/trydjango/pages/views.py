1d7e6503f918e24d68e976aeb091085f3489c99f
Victor Hidalgo <33233227+Victor-Hidalgo@users.noreply.github.com>
2024-06-10 18:28:52 +0100

    Add files via upload

--- 2024/django-trial/src/trydjango/pages/views.py
+++ 2024/django-trial/src/trydjango/pages/views.py
@@ -2,5 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
--- 2024/django-trial/src/trydjango/trydjango/urls.py
+++ 2024/django-trial/src/trydjango/trydjango/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view
+from pages.views import home_view, contact_view, about_view, social_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('about/', about_view),
+    path('contact/', contact_view),
+    path('social/', social_view),
     path('admin/', admin.site.urls),
 ]
